1ef0db8b1de620cec1928da8b37c51aefd7d959a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-10 21:25:26 +0300

    Revert "15: enable signature matching of updated system apps for all such apps"
    It's redundant and causes issues with sharedUid third-party apps such as GSF+GmsCore.

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -4325,8 +4325,8 @@ final class InstallPackageHelper {
         if (!android.security.Flags.extendVbChainToUpdatedApk()) {
             return false;
         }
-
-        return true;
+        return mPm.mInjector.getSystemConfig().getPreinstallPackagesWithStrictSignatureCheck()
+            .contains(packageName);
     }
 
     /**
